99da24cf43376b731e9aa838fef33cfc225dffae
Lo Hong Yun <hylo@cass.myead.dir>
2020-09-11 17:02:50 +0800

    add @angular/material, @angular/cdk, and @angular/service-worker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'xiaoyue-angular';
+export class AppComponent implements OnInit {
+
+  title = 'LINE 小月';
+
+  public constructor(private titleService: Title ) { }
+  ngOnInit(): void {
+    this.setTitle(this.title);
+  }
+
+  public setTitle( newTitle: string) {
+    this.titleService.setTitle( newTitle );
+  }
+
+
 }
