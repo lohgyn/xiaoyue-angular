f77d65f0b8e38c85e09384ae44ea6e89a26bb718
Hong Yun <lohongyun1991@hotmail.com>
2020-09-12 14:54:28 +0800

    update package.json to build app with production config

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const port = 8080;
 
 app.use(express.static(path.join(__dirname, 'dist/xiaoyue-angular')));
 
@@ -8,4 +10,6 @@ app.get("/*", function (req, res) {
   res.sendFile("index.html", { root: "dist/xiaoyue-angular" });
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || port, () => {
+    console.log(`xiaoyue-angular app listening at http://localhost:${port}`)
+});
